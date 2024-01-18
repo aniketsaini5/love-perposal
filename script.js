01ff7d71fbb872d27e102016a5e7fac0a999d4cd
Aniket Saini <154746704+aniketsaini5@users.noreply.github.com>
2024-01-18 17:07:21 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".title").click(function () {
+      $(".container").addClass("open");
+    });
+  
+    $(".close").click(function () {
+      $(".container").removeClass("open");
+    });
+  });
+
+
+
